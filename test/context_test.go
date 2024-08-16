5d6cc7bc2dfeef5cbac3dabe13d3dccb1a908cb3
wushuaiqi1 <wushuaiqi1@tal.com>
2024-08-16 17:35:31 +0800

    commit

---
+++ test/context_test.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"context"
+	"testing"
+)
+
+func TestContextWithValue(t *testing.T) {
+	ctx := context.Background()
+	res := ctx.Value("111")
+	println(res)
+}
+
+func TestContextTODO(t *testing.T) {
+
+}
---
+++ test/interface_test.go
@@ -0,0 +1,38 @@
+package test
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+}
+
+func NewPerson() *Person {
+	return nil
+}
+
+func (person Person) Read(p []byte) (n int, err error) {
+	return
+}
+
+func TestInterfaceNil(t *testing.T) {
+	// 注意 go中判空需要值和类型都为nil
+	var p io.Reader = NewPerson()
+	fmt.Println(p == nil)
+	fmt.Println(reflect.ValueOf(p).IsNil())
+	fmt.Println(reflect.TypeOf(p))
+
+	// 捕获异常～
+	defer func() {
+		if ok := recover(); ok != nil {
+			fmt.Println(ok)
+		}
+	}()
+	_, err := p.Read([]byte("111"))
+	if err != nil {
+		return
+	}
+}
