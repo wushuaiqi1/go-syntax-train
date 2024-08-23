6bda69f83d5a6b0773936954a38ae4506af2d0b8
wushuaiqi1 <wushuaiqi1@tal.com>
2024-08-23 18:26:55 +0800

    压测数据库连接树

--- test/model/db_message_test.go
+++ test/model/db_message_test.go
@@ -3,8 +3,11 @@ package model
 import (
 	"go-syntax-train/cmd"
 	"go-syntax-train/model"
+	"log"
 	"strconv"
+	"sync"
 	"testing"
+	"time"
 )
 
 // 模型映射表
@@ -17,9 +20,19 @@ func TestAutoMigrate(t *testing.T) {
 }
 
 func TestBatchCreate(t *testing.T) {
+	start := time.Now().UnixMilli()
 	db := cmd.ConnectTest()
-	for i := 0; i < 100; i++ {
-		message := model.NewMessage("我是消息"+strconv.Itoa(i), 1)
-		db.Create(&message)
+	res := sync.WaitGroup{}
+	for i := 1; i <= 151; i++ {
+		res.Add(1)
+		go func(i int) {
+			message := model.NewMessage("我是消息"+strconv.Itoa(i), 1)
+			db.Create(&message)
+			res.Add(-1)
+		}(i)
 	}
+	// 阻塞式等待
+	res.Wait()
+	end := time.Now().UnixMilli()
+	log.Println("批量插入数据耗时：", end-start, " ms")
 }
